304ba2169d416684c8bd94c9825b401813c70883
onei2 <70448706+onei2@users.noreply.github.com>
2020-09-01 02:06:53 +0200

    Update onei2geo.sql

--- onei2geo.sql
+++ onei2geo.sql
@@ -27,6 +27,25 @@ SET d = 6378137 * SQRT(x * x + y * y);
 RETURN ROUND(d);
 END $$
 
+CREATE FUNCTION EquirectangularApproximationSquared (lat1 DOUBLE, lng1 DOUBLE, lat2 DOUBLE, lng2 DOUBLE) RETURNS DOUBLE
+BEGIN
+DECLARE x DOUBLE;
+DECLARE y DOUBLE;
+DECLARE d DOUBLE;
+DECLARE lat1rad DOUBLE;
+DECLARE lng1rad DOUBLE;
+DECLARE lat2rad DOUBLE;
+DECLARE lng2rad DOUBLE;
+SET lat1rad = RADIANS(lat1);
+SET lng1rad = RADIANS(lng1);
+SET lat2rad = RADIANS(lat2);
+SET lng2rad = RADIANS(lng2);
+SET x = (lng2rad - lng1rad) * COS((lat1rad + lat2rad) / 2);
+SET y = lat2rad - lat1rad;
+SET d = 6378137 * (x * x + y * y);
+RETURN ROUND(d);
+END $$
+
 CREATE FUNCTION SphericalLawOfCosines (lat1 DOUBLE, lng1 DOUBLE, lat2 DOUBLE, lng2 DOUBLE) RETURNS DOUBLE
 BEGIN
 DECLARE d DOUBLE;
